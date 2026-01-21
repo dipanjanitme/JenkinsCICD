e777e03ba2ce3c09c6bf53ca16f4d66e37423656
dipanjanitme <dipanjanitme@gmail.com>
2026-01-20 19:05:41 -0600

    Add parameters to printOut.ps1 for name and manager status

--- script/printOut.ps1
+++ script/printOut.ps1
@@ -1,2 +1,8 @@
+parameter(
+[string] myName,
+[bool]isManager
+)
 Write-OutPut "I am working on creating my first ever Jenkins pipeline"
 Write-OutPut "Env Variable is: $env:ENV"
+Write-OutPut "My name is $myName"
+Write-OutPut "Am I a manager: $isManager"
