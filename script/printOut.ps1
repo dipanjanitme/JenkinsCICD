d4335809a70d2388737719a040e485ca19ea51cf
dipanjanitme <dipanjanitme@gmail.com>
2026-01-20 19:06:19 -0600

    Fix variable casing in printOut.ps1

--- script/printOut.ps1
+++ script/printOut.ps1
@@ -4,5 +4,5 @@ parameter(
 )
 Write-OutPut "I am working on creating my first ever Jenkins pipeline"
 Write-OutPut "Env Variable is: $env:ENV"
-Write-OutPut "My name is $myName"
+Write-OutPut "My name is $myname"
 Write-OutPut "Am I a manager: $isManager"
