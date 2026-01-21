dbee5f7d5370610f9108fe4874e01292db15a9e7
dipanjanitme <dipanjanitme@gmail.com>
2026-01-20 19:10:46 -0600

    Fix parameter name case and update output messages

--- script/printOut.ps1
+++ script/printOut.ps1
@@ -1,8 +1,8 @@
 parameter(
-[string] myName,
+[string] myname,
 [bool]isManager
 )
 Write-OutPut "I am working on creating my first ever Jenkins pipeline"
-Write-OutPut "Env Variable is: $env:ENV"
+Write-OutPut "Env Variable in powershell script is: $env:ENV"
 Write-OutPut "My name is $myname"
 Write-OutPut "Am I a manager: $isManager"
